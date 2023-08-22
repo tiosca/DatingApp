7975b6ea329a5361744dee4d1dd56342af2c7a20
VitalieNanu <vnanu@insyncgo.com>
2023-08-22 16:17:51 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,15 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
+
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+//Configure the HTTP request pipeline.
+app.UseCors(builder=>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
