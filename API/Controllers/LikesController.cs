3b0a49bc9b412a8787dceaf943d196b563db1abf
VitalieNanu <vnanu@insyncgo.com>
2023-09-03 22:32:52 +0300

    Implemented the unit of work design pattern

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -9,13 +9,11 @@ namespace API.Controllers
 {
     public class LikesController : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
-        private readonly ILikesRepository _likesRepository;
+        private readonly IUnitOfWork _uow;
 
-        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        public LikesController(IUnitOfWork uow)
         {
-            _userRepository = userRepository;
-            _likesRepository = likesRepository;
+            _uow = uow;
         }
 
         [HttpPost("{username}")]
@@ -23,13 +21,13 @@ namespace API.Controllers
         {
             var sourceUserId = User.GetUserId();
 
-            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
-            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+            var likedUser = await _uow.UserRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _uow.LikesRepository.GetUserWithLikes(sourceUserId);
 
             if (likedUser == null) return NotFound();
             if (sourceUser.UserName == username) return BadRequest("You cannot like yourself!");
 
-            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+            var userLike = await _uow.LikesRepository.GetUserLike(sourceUserId, likedUser.Id);
             if (userLike != null) return BadRequest("You already liked this user");
 
             userLike = new UserLike
@@ -38,7 +36,7 @@ namespace API.Controllers
                 TargetUserId = likedUser.Id
             };
             sourceUser.LikedUsers.Add(userLike);
-            if (await _userRepository.SaveAllAsync()) return Ok();
+            if (await _uow.Complete()) return Ok();
 
             return BadRequest("Failed to like user");
         }
@@ -48,7 +46,7 @@ namespace API.Controllers
         {
             likesParams.UserId = User.GetUserId();
 
-            var users = await _likesRepository.GetUserLikes(likesParams);
+            var users = await _uow.LikesRepository.GetUserLikes(likesParams);
 
             Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages));
 
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -33,10 +33,6 @@ namespace API.Data
         {
             return await _context.Users.Include(p=>p.Photos).SingleOrDefaultAsync(x => x.UserName == username);
         }
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
         public void Update(AppUser user)
         {
             _context.Entry(user).State = EntityState.Modified;
@@ -68,6 +64,11 @@ namespace API.Data
         public async Task<MemberDto> GetMemberAsync(string username)
         {
             return await _context.Users.Where(x => x.UserName == username).ProjectTo<MemberDto>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+        }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users.Where(x => x.UserName == username).Select(x => x.Gender).FirstOrDefaultAsync();
         }
     }
 }
